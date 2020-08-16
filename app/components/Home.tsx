4b3a1331367edcf69425837fb4c1ba01eaccaa30
Casaubon <netzach.yoshida@gmail.com>
2020-08-17 00:22:36 +1200

    Fix height because of second time in same property

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -5,9 +5,20 @@ import styles from './Home.css';
 
 export default function Home(): JSX.Element {
   return (
-    <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
+    // <div className={styles.container} data-tid="container">
+    //   <h2>Emoji Picker</h2>
+    //   <Link to={routes.COUNTER}>to Counter</Link>
+    <div className={styles.form__group}>
+      {/* <i className="fas fa-search search_icon" /> */}
+      <label className={styles.form__label} htmlFor="word input">
+        <input
+          id="word"
+          className={styles.form__input}
+          type="text"
+          placeholder="😊"
+        />
+      </label>
+      {/* <i className="fas fa-times search_icon" /> */}
     </div>
   );
 }
